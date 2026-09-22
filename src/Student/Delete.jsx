66510b5103335b0a2ca94b5713572849bbe3f017
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-09-22 19:57:42 +0530

    front-end

--- src/Student/Delete.jsx
+++ src/Student/Delete.jsx
@@ -17,13 +17,13 @@ const Delete = () => {
 
          BackendAi.delete(getId.stuId)
          .then((e) => {
-            alert("UPDATE SUCCESS!!");
+            alert("DELETE SUCCESS!!");
             console.log("DELETE SUSSESS:", e)
              usenagivate("/fetch");
              setId(({stuId:""}))
             })
          .catch((e) => {
-            alert("UPDATE ERROR!!")
+            alert("DELETE ERROR!!")
             console.log("DELETE ERROR:",e)
             });
     }
